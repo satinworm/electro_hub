6a84cb53229a09332ed57d8d3d225e11fb7aa8d4
Ilya Burch <satinworm@gmail.com>
2024-08-21 12:17:14 +0300

    removed en locale

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,14 +1,13 @@
-import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
-import { Messages } from '../global';
+import { getRequestConfig } from "next-intl/server";
+import { notFound } from "next/navigation";
+import type { Messages } from "../global";
 
-const locales = ['ru', 'en'];
-const defaultLocale = 'ru';
+const locales = ["ru"];
+const defaultLocale = "ru";
 
 export default getRequestConfig(async ({ locale }) => {
-    if (!locales.includes(locale as any)) notFound();
-    return {
-        messages: (await import(`../messages/${locale}.json`))
-            .default as Messages,
-    };
+	if (!locales.includes(locale as any)) notFound();
+	return {
+		messages: (await import(`../messages/${locale}.json`)).default as Messages,
+	};
 });
