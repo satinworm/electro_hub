2c9bc813a7e809576dac171b87daaea8a8b5d515
Ilya Burch <satinworm@gmail.com>
2025-06-19 11:06:42 +0300

    a

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,32 +4,32 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: false,
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	images: {
-		domains: [
-			"localhost",
-			"localhost:1349",
-			"electrohub.by",
-			"strapi.electrohub.by",
-		],
-	},
-	generateBuildId: async () => {
-		return process.env.GIT_HASH;
-	},
-	env: {
-		// NEXT_PUBLIC_PUBLIC_URL: "http://localhost:3009",
-		// NEXT_PUBLIC_SERVER_URL: "https://strapi.electrohub.by",
-		// NEXT_PUBLIC_BOT_URL: "https://electrohub.by",
-		// GIT_HASH: "1",
-	},
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        domains: [
+            "localhost",
+            "localhost:1349",
+            "electrohub.by",
+            "strapi.electrohub.by",
+        ],
+    },
+    generateBuildId: async () => {
+        return process.env.GIT_HASH;
+    },
+    env: {
+        NEXT_PUBLIC_PUBLIC_URL: "https://electrohub.by",
+        NEXT_PUBLIC_SERVER_URL: "https://strapi.electrohub.by",
+        NEXT_PUBLIC_BOT_URL: "https://electrohub.by",
+        GIT_HASH: "1",
+    },
 
-	experimental: {
-		// …
-		serverComponentsExternalPackages: ["@react-pdf/renderer"],
-	},
+    experimental: {
+        // …
+        serverComponentsExternalPackages: ["@react-pdf/renderer"],
+    },
 };
 
 export default withNextIntl(nextConfig);
